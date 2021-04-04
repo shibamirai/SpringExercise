67b11ac6564657a53d25c5977533c9495cd3a454
shibamirai <66295034+shibamirai@users.noreply.github.com>
2021-04-04 15:22:38 +0900

    EX3

--- SpringExercise/src/main/java/katachi/spring/exercise/controller/InputController.java
+++ SpringExercise/src/main/java/katachi/spring/exercise/controller/InputController.java
@@ -1,15 +1,22 @@
 package katachi.spring.exercise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/input")
 public class InputController {
 
 	@GetMapping
-	public String show() {
+	public String show(
+			@RequestParam(name="name", required=false)
+			String name,
+			Model model
+	) {
+		model.addAttribute("name", name);
 		return "input";
 	}
 }
