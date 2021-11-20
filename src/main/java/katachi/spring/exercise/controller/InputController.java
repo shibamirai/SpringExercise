00885710c53011000ef402ab04abc90555efed36
shibamirai <66295034+shibamirai@users.noreply.github.com>
2021-11-20 09:12:55 +0900

    EX3. GETパラメータ

--- src/main/java/katachi/spring/exercise/controller/InputController.java
+++ src/main/java/katachi/spring/exercise/controller/InputController.java
@@ -1,15 +1,20 @@
 package katachi.spring.exercise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/input")
 public class InputController {
 
 	@GetMapping
-	public String getInput() {
+	public String getInput(@RequestParam(name="name", required=false) String name, Model model) {
+
+		model.addAttribute("name", name);
+
 		return "input";
 	}
 }
