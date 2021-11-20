62abd266d7f23c9c7139451298f62c99964b9f3f
shibamirai <66295034+shibamirai@users.noreply.github.com>
2021-11-20 10:30:09 +0900

    EX4. POST

--- src/main/java/katachi/spring/exercise/controller/InputController.java
+++ src/main/java/katachi/spring/exercise/controller/InputController.java
@@ -3,6 +3,7 @@ package katachi.spring.exercise.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,5 +17,13 @@ public class InputController {
 		model.addAttribute("name", name);
 
 		return "input";
+	}
+
+	@PostMapping
+	public String postInput(@RequestParam("name") String name, Model model) {
+
+		model.addAttribute("name", name);
+
+		return "profile";
 	}
 }
